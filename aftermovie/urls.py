8ba4114c8e55932a08ef42b3143e0de618566f95
jungmannn <task11@mju.ac.kr>
2019-07-27 12:53:51 +0900

    ma2n3 commit

--- aftermovie/urls.py
+++ aftermovie/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import blog.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name = 'home'),
-]
+    path('board/', blog.views.board, name = 'board'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render#, get_object_or_404, redirect
+from .models import Blog
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+def board(request):
+    blogs = Blog.objects
+    return render(request, 'board.html', {'blogs' : blogs})
